58b699fb50278eecbc57dda01cc0158a5bec3a87
Alex Clark <alexclark4121@gmail.com>
2019-09-22 13:11:44 -0400

    set up first storyboard, UITableView

--- imageViewer/ViewController.swift
+++ imageViewer/ViewController.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: <#T##String#>)
+        let items = try! fm.contentsOfDirectory(atPath: path)
         for item in items {
             if item.hasPrefix("nssl") {
-                
+                pictures.append(item)
             }
+            print(pictures)
         }
         // Do any additional setup after loading the view.
     }
